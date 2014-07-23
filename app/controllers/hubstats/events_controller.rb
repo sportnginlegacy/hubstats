0957ee69ceea7444ac0d8a44a97cc2454d0e24f5
elliothursh <elliothursh@gmail.com>
2014-07-23 10:22:43 -0500

    moved things into verify signature

--- app/controllers/hubstats/events_controller.rb
+++ app/controllers/hubstats/events_controller.rb
@@ -4,9 +4,7 @@ module Hubstats
   class EventsController < ApplicationController
 
     def handler
-      request.body.rewind
-      payload_body = request.body.read
-      verify_signature(payload_body)
+      verify_signature(request)
 
       kind = request.headers['X-Github-Event']
       event = params.with_indifferent_access
@@ -17,7 +15,9 @@ module Hubstats
     end
 
     private
-    def verify_signature(payload_body)
+    def verify_signature(request)
+      request.body.rewind
+      payload_body = request.body.read
       signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), Hubstats.config.webhook_endpoint, payload_body)
       return 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
     end
