8752e6bb09d7479fbbd82fe9b831f51032de014b
Emma Sax <emma.sax4@gmail.com>
2015-05-22 16:19:27 -0500

    made the basics of a deploy controller, view, and model, as well as made a router to it and it successfully leads to a page, but near end of day began having trouble migrating information from github. Don't know how to fix that... normal commands aren't working

--- app/controllers/hubstats/deploys_controller.rb
+++ app/controllers/hubstats/deploys_controller.rb
@@ -4,18 +4,12 @@ module Hubstats
   class DeploysController < ApplicationController
 
 # I'll probably need to update the users controller, model, as well so that we can access their deploys
-  	def index
-  		@git_rivision = nil
-  		@repo_id = nil
-  		@deployed_at = nil
-  		@deployed_by = nil
-  		@user = nil
-  		@repo = nil
-  		@pull_requests = []
-  	end
+    def index
+      @deploys = Deploy.all
+    end
 
-  	def show
-  	end
+    def show
+    end
 
   end
  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 Hubstats::Engine.routes.draw do
   root to: "pull_requests#index"
 
-  post "/handler" => "events#handler", :as => "handler"
-  get "/metrics" => "repos#dashboard", :as => "metrics" 
+  post "/handler" => "events#handler", :as => :handler
+  get "/metrics" => "repos#dashboard", :as => :metrics 
   get "/pulls" => "pull_requests#index", :as => :pulls
+  get "/deploys" => "deploys#index", :as => :deploys
   get "/users" => "users#index", :as => :users
   get "/repos" => "repos#index", :as => :repos
   get "/user/:id" => "users#show", :as => :user
   get "/:repo" => "repos#show", :as => :repo
-  get "/deploys" => "deploys#index", as => :deploys
   scope "/:repo", :as => :repo do
     get '/pull/:id' => "pull_requests#show", :as => :pull
   end
-
-  #handler/deployment
 
 end
