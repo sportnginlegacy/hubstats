7d33c26744e5060439fc5bc879450a0129058dca
Emma Sax <emma.sax4@gmail.com>
2015-05-26 14:33:09 -0500

    Have the basics of a deploys page, need to add actual deploy data

--- app/controllers/hubstats/deploys_controller.rb
+++ app/controllers/hubstats/deploys_controller.rb
@@ -4,7 +4,24 @@ module Hubstats
   class DeploysController < ApplicationController
 
 # I'll probably need to update the users controller, model, as well so that we can access their deploys
-    def index
+   def index
+    #	deploy_ids = Hubstats::Deploy
+    #	  .belonging_to(params[:users])
+    #	  .belonging_to(params[:repos])
+    #	  .state_based_order(@timespan,params[:state],"ASC")
+    #	  .map(&:id)
+
+    #	@deploys = Hubstats::Deploy.includes(:user).includes(:repo)
+    #	  .belonging_to_users(params[:users]).belonging_to_repos(params[:repos])
+    #	  .group_by(params[:group])
+    #	  .state_based_order(@timespan,params[:state],params[:order])
+    #	  .paginate(:page => params[:page], :per_page => 15)
+
+    #	if params[:group] == 'user'
+    #	  @groups = @deploys.to_a.group_by(&:user_name)
+    #	elsif params[:group] == 'repo'
+    #	  @groups = @deploys.to_a.group_by(&:repo_name)
+    #	end
       @deploys = Deploy.all
     end
 
---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Deploy.create(git_revision: "c1a2b37", repo_id: 47, deployed_at: "2009-12-03 18:00:00 -0500", 
+	deployed_by: "emmasax1")
+Deploy.create(git_revision: "i32jb9e", repo_id: 92, deployed_at: "2010-05-26 17:00:00 -0300",
+	deployed_by: "aplackner")
