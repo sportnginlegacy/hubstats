4f07b8053fd278f10748353e5efab13fb0bf8b19
Emma Sax <emma.sax4@gmail.com>
2015-06-03 11:01:04 -0500

    once more, didn't save

--- app/assets/javascripts/hubstats/application.js
+++ app/assets/javascripts/hubstats/application.js
@@ -4,10 +4,10 @@
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
 //
-// It"s not advisable to add code directly here, but if you do, it"ll appear at the bottom of the
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT"S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
