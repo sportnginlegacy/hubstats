4ea906c5470f4324b43756bb6b2d2e7d148fc318
Emma Sax <emma.sax4@gmail.com>
2015-06-12 16:04:18 -0500

    deleted duplicate of grouping method

--- app/controllers/hubstats/application_controller.rb
+++ app/controllers/hubstats/application_controller.rb
@@ -8,13 +8,5 @@ module Hubstats
       cookies[:hubstats_index] ||= 2
       @timespan = TIMESPAN_ARRAY[cookies[:hubstats_index].to_i][:date].ago.to_date
     end
-
-    def grouping (group_request, group)
-      if group_request == "user"
-        @groups = group.to_a.group_by(&:user_name)
-      elsif group_request == "repo"
-        @groups = group.to_a.group_by(&:repo_name)
-      end
-    end
   end
 end
