00b15329c07866a08914422a486c6ec826f6c2f4
Emma Sax <emma.sax4@gmail.com>
2015-06-16 14:29:11 -0500

    adding back in state_based_order

--- app/controllers/hubstats/pull_requests_controller.rb
+++ app/controllers/hubstats/pull_requests_controller.rb
@@ -9,6 +9,7 @@ module Hubstats
       pull_ids = Hubstats::PullRequest
         .belonging_to_users(params[:users])
         .belonging_to_repos(params[:repos])
+        .state_based_order(@timespan,params[:state],"ASC")
         .map(&:id)
 
       @labels = Hubstats::Label.with_a_pull_request(pull_ids).order("pull_request_count DESC")
