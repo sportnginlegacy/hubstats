db239261d6a5f8bb15fde8333986504127663e6c
Emma Sax <emma.sax4@gmail.com>
2015-06-24 14:43:42 -0500

    found one last mistake...

--- app/controllers/hubstats/application_controller.rb
+++ app/controllers/hubstats/application_controller.rb
@@ -5,12 +5,12 @@ module Hubstats
 
     private
     def set_time
-      if cookies[:hubstats_index] == "null~~null" || cookies[:hubstats_index] == nil
+      if cookies[:hubstats_dates] == "null~~null" || cookies[:hubstats_dates] == nil
         @start_date = Date.today - 14
         @end_date = Date.today + 1
       else
-        @start_date = cookies[:hubstats_index].split("~~").first.to_date
-        @end_date = cookies[:hubstats_index].split("~~").last.to_date + 1
+        @start_date = cookies[:hubstats_dates].split("~~").first.to_date
+        @end_date = cookies[:hubstats_dates].split("~~").last.to_date + 1
       end
     end
   end
