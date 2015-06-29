76ecad6bb76c79b5e9d5b6ec8443f37253135e1c
Emma Sax <emma.sax4@gmail.com>
2015-06-29 13:18:15 -0500

    commented out routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
+# Draw all of the routes that will be used in Hubstats
 Hubstats::Engine.routes.draw do
-  root :to => "pull_requests#index" # sets default root to be the pulls page
+  root to: "pull_requests#index" # sets default root to be the pulls page
   post "/handler" => "events#handler", :as => :handler
   resources :deploys, :only => [:create, :index, :show] # routes to index, show, and to create method
   get "/metrics" => "repos#dashboard", :as => :metrics # routes to list of repos and stats
@@ -9,6 +10,6 @@ Hubstats::Engine.routes.draw do
   get "/repos" => "repos#index", :as => :repos # route is for the repo filter on the pull request and deploys page
   get "/:repo" => "repos#show", :as => :repo # routes to specific repo's stats
   scope "/:repo", :as => :repo do
-    get '/pull/:id' => "pull_requests#show", :as => :pull
+    get '/pull/:id' => "pull_requests#show", :as => :pull # routes to the specific repo's pull id
   end
 end
