5f1bca0404c597b915658b905089a3bef714e613
Emma Sax <emma.sax4@gmail.com>
2015-07-07 15:02:46 -0500

    uncommenting out new tests...

--- spec/lib/hubstats_spec.rb
+++ spec/lib/hubstats_spec.rb
@@ -1,20 +1,20 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe Hubstats
-#   context ".config" do
-#     subject { Hubstats }
-#     after do
-#       Hubstats.class_variable_set(:@@config, nil)
-#     end
-#     it "creates a new config object" do
-#       expect(Hubstats::Config).to receive(:parse).once { double(:config) }
-#       subject.config
-#     end
+describe Hubstats
+  context ".config" do
+    subject { Hubstats }
+    after do
+      Hubstats.class_variable_set(:@@config, nil)
+    end
+    it "creates a new config object" do
+      expect(Hubstats::Config).to receive(:parse).once { double(:config) }
+      subject.config
+    end
 
-#     it "memorizes the config object" do
-#       expect(Hubstats::Config).to receive(:parse).once { double(:config) }
-#       subject.config
-#       subject.config
-#     end
+    it "memorizes the config object" do
+      expect(Hubstats::Config).to receive(:parse).once { double(:config) }
+      subject.config
+      subject.config
+    end
 
-#   end
+  end
