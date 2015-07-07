3887df939a14233f3d2d5700e5646fc4569e955e
Andy Fleener <andrew.fleener@sportngin.com>
2015-07-07 13:23:18 -0500

    Fix race condition in the config test

--- spec/lib/hubstats_spec.rb
+++ spec/lib/hubstats_spec.rb
@@ -2,15 +2,19 @@ require 'spec_helper'
 
 describe Hubstats
   context ".config" do
+    subject { Hubstats }
+    after do
+      Hubstats.class_variable_set(:@@config, nil)
+    end
     it "creates a new config object" do
-      expect(Hubstats::Config).to receive(:parse).once
-      Hubstats.config
+      expect(Hubstats::Config).to receive(:parse).once { double(:config) }
+      subject.config
     end
 
     it "memoizes the config object" do
       expect(Hubstats::Config).to receive(:parse).once { double(:config) }
-      Hubstats.config
-      Hubstats.config
+      subject.config
+      subject.config
     end
 
   end
