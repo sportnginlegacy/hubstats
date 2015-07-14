9563cf5d022dc5e1d545f5415dc2b3d4ae56b8a5
Emma Sax <emma.sax4@gmail.com>
2015-07-14 14:00:13 -0500

    Fixed team show page so that users' mini metrics show up and the description of the team shows up

--- app/controllers/hubstats/teams_controller.rb
+++ app/controllers/hubstats/teams_controller.rb
@@ -26,7 +26,7 @@ module Hubstats
       @team = Hubstats::Team.where(id: params[:id]).first
       @pull_requests = Hubstats::PullRequest.belonging_to_team(@team.id).merged_in_date_range(@start_date, @end_date).order("updated_at DESC").limit(20)
       @pull_count = Hubstats::PullRequest.belonging_to_team(@team.id).merged_in_date_range(@start_date, @end_date).count(:all)
-      @users = @team.users.limit(20)
+      @users = @team.users
       @user_count = @team.users.length
       @comment_count = Hubstats::Comment.belonging_to_team(@users.pluck(:id)).created_in_date_range(@start_date, @end_date).count(:all)
       @net_additions = Hubstats::PullRequest.merged_in_date_range(@start_date, @end_date).belonging_to_team(@team.id).sum(:additions).to_i -
