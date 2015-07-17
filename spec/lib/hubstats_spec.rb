07981c7026a5437723837f4d7d5be3f88352c44d
Emma Sax <emma.sax4@gmail.com>
2015-07-17 09:08:29 -0500

    made all tests pass; getting rid of comparing user names to hubstats to promote open-source happiness

--- spec/lib/hubstats_spec.rb
+++ spec/lib/hubstats_spec.rb
@@ -7,14 +7,18 @@ describe Hubstats
     before do
       Hubstats.class_variable_set(:@@config, nil)
     end
+
+    after do
+      Hubstats.remove_class_variable(:@@config)
+    end
     
     it "creates a new config object" do
-      expect(Hubstats::Config).to receive(:parse).at_least(:once) { double(:config) }
+      expect(Hubstats::Config).to receive(:parse).once { double(:config) }
       subject.config
     end
 
     it "memorizes the config object" do
-      expect(Hubstats::Config).to receive(:parse).at_least(:once) { double(:config) }
+      expect(Hubstats::Config).to receive(:parse).once { double(:config) }
       subject.config
       subject.config
     end
