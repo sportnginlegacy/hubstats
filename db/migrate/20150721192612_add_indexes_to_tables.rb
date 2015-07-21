8b460e97d1292b1b18817f7cd28f6845fb46b9af
Emma Sax <emma.sax4@gmail.com>
2015-07-21 14:38:13 -0500

    adding indexes where they should exist

---
+++ db/migrate/20150721192612_add_indexes_to_tables.rb
@@ -0,0 +1,15 @@
+class AddIndexesToTables < ActiveRecord::Migration
+  def change
+    add_index :hubstats_deploys, :repo_id
+    add_index :hubstats_deploys, :user_id
+
+    add_index :hubstats_labels_pull_requests, :pull_request_id
+    add_index :hubstats_labels_pull_requests, :label_id
+
+    add_index :hubstats_pull_requests, :deploy_id
+    add_index :hubstats_pull_requests, :team_id
+
+    add_index :hubstats_teams_users, :user_id
+    add_index :hubstats_teams_users, :team_id
+  end
+end
