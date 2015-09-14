c94de611cb775068e79260974324e7ddab55d3a8
Brian Bergstrom <brian.bergstrom@sportngin.com>
2015-09-14 16:07:54 -0500

    mysql2 version restriction needs to be in gemspec.

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150721193128) do
+ActiveRecord::Schema.define(:version => 20150721192612) do
 
   create_table "hubstats_comments", :force => true do |t|
     t.string   "kind"
