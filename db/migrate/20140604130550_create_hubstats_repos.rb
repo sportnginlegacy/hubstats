0b9d50b00fc91852c25b92d3bfd6519292510c6d
Emma Sax <emma.sax4@gmail.com>
2016-01-07 14:59:13 -0600

    Now that should be it

--- db/migrate/20140604130550_create_hubstats_repos.rb
+++ db/migrate/20140604130550_create_hubstats_repos.rb
@@ -1,7 +1,6 @@
 class CreateHubstatsRepos < ActiveRecord::Migration
   def change
     create_table :hubstats_repos do |t|
-
       t.belongs_to :owner
       t.string :name
       t.string :full_name
