79e2fed60371797eea9f56d67b15ed4ad1578812
Emma Sax <emma.sax4@gmail.com>
2016-01-08 13:25:45 -0600

    Last editing of config/environments files before next upgrading

--- test/dummy/config/environments/development.rb
+++ test/dummy/config/environments/development.rb
@@ -5,7 +5,6 @@ Dummy::Application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
   config.eager_load = false
 
   # Show full error reports and disable caching
