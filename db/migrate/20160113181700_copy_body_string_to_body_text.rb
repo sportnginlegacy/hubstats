4acad9b197bcc920124f708727db8253cb661366
Emma Sax <emma.sax4@gmail.com>
2016-01-13 12:54:46 -0600

    Changing change to be self.up and adding self.down

--- db/migrate/20160113181700_copy_body_string_to_body_text.rb
+++ db/migrate/20160113181700_copy_body_string_to_body_text.rb
@@ -1,5 +1,9 @@
 class CopyBodyStringToBodyText < ActiveRecord::Migration
-  def change
+  def self.up
      Hubstats::Comment.update_all("body_text=body")
+  end
+
+  def self.down
+  	Hubstats::Comment.update_all("body=body_text")
   end
 end
