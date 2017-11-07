231ed8d0dc03aa1f51e3295b2e6fa3acce2b4589
Elliott Beach <elliott2.71828@gmail.com>
2017-11-06 22:30:14 -0600

    improve code style

--- db/migrate/20171104172008_handle_emojis_in_comments.rb
+++ db/migrate/20171104172008_handle_emojis_in_comments.rb
@@ -1,6 +1,10 @@
 class HandleEmojisInComments < ActiveRecord::Migration
   def change
-      execute("ALTER TABLE hubstats_comments MODIFY body TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;")
+
+      # Set the body column to use 4-byte characters so that 4-byte UTF-8 chars, such as emojis, can be stored.
+      change_column :hubstats_comments, :body, "TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
+
+      # Set the connection to use a 4-byte encoding as well.
       execute("set names 'utf8mb4';")
   end
 end
