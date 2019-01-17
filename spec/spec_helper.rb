28dad8fa9a11ea04f368e4e5fd11d963daded185
Emma Sax <emma.sax4@gmail.com>
2019-01-17 15:16:27 -0600

    Revert "Don't specify this here"
    This reverts commit f7d703a22d0fb3ee9912abcf8f0551c1d0ca9f4d.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../test/dummy/config/environment", __FILE__)
 require 'rspec/rails'
+require 'factory_bot'
 require 'faker'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
