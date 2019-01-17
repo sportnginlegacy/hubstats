29d04fcad544d0e1c6a222e28acdfbe6c6a6bcf2
Emma Sax <emma.sax4@gmail.com>
2019-01-17 15:16:38 -0600

    Revert "Try this"
    This reverts commit d51b49886cd9c6a593f5b50b8d58b531f9251aa3.

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -16,14 +16,14 @@ FactoryGirl.define do
     name "Team One"
     hubstats true
     action "added"
-    initialize_with { attributes }
+    initialize_with { attributes } 
   end
 
-  factory :team_payload_hash, class:Hash do
+  factory :team_payload_hash, class:Hash do 
     id {Faker::Number.number(6).to_i}
     type "MembershipEvent"
     association :user, factory: :user_hash, strategy: :build
     association :team, factory: :team_hash, strategy: :build
-    initialize_with { attributes }
-  end
+    initialize_with { attributes } 
+  end 
 end
