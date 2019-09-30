41b9e27310719dddebbd54c3ac1871a415734cd1
Emma Sax <emma.sax4@gmail.com>
2019-09-30 16:13:37 -0500

    Rename Dummy::Application to Rails.application

--- test/dummy/config/initializers/secret_token.rb
+++ test/dummy/config/initializers/secret_token.rb
@@ -4,5 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '8fc9a32d6e2d0d41ea3cd968e27cfee1e819a8516920a9cad3ec6a000ddfe8559a55752f9566d8cebba4466844e0790c0d664c2c989bb253925f4713ff2609f5'
-Dummy::Application.config.secret_key_base = 'c598609edbe079079483ddc558a997ce28cc6ca3ae3d03a7332e2037292a4b99aeff5827c3f768995843ffbb94032b5b0852ff332f137aa1485c8c65773050eb'
+Rails.application.config.secret_token = '8fc9a32d6e2d0d41ea3cd968e27cfee1e819a8516920a9cad3ec6a000ddfe8559a55752f9566d8cebba4466844e0790c0d664c2c989bb253925f4713ff2609f5'
+Rails.application.config.secret_key_base = 'c598609edbe079079483ddc558a997ce28cc6ca3ae3d03a7332e2037292a4b99aeff5827c3f768995843ffbb94032b5b0852ff332f137aa1485c8c65773050eb'
