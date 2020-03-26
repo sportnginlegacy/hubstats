f1e6610af004ca6e57f29baf4b9dd4269d549eb1
Emma Sax <emma.sax@sportsengine.com>
2020-03-26 12:05:23 -0500

    Add the monkey patch for pairing Rails 4 with Ruby 2.6.x

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,8 @@ require File.expand_path("../../test/dummy/config/environment", __FILE__)
 require 'rspec/rails'
 require 'factory_girl_rails'
 require 'faker'
+require 'test_response_patch'
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -23,7 +25,7 @@ RSpec.configure do |config|
   # config.mock_with :rr
 
   config.include FactoryGirl::Syntax::Methods
-  
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
